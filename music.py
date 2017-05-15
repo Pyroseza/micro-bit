9a8da955e250fca5d1603121474fb30587218f16
Pyroseza <jarpri08@gmail.com>
2017-05-15 22:21:41 +0100

    added dancing lights
    Found a cool script that makes the LEDs "dance" I thought it would be appropriate for the buzzer tunes d^_^b

--- music.py
+++ music.py
@@ -1,3 +1,4 @@
+import microbit
 import music
 import random
 
@@ -8,9 +9,16 @@ numbersLow = ["0", "1", "2"]
 numbersHigh = ["3", "4", "5"]
 
 def main():
+    dots = [ [0]*5, [0]*5, [0]*5, [0]*5, [0]*5 ]
     while True:
+        dots[random.randrange(5)][random.randrange(5)] = 8
+        for i in range(5):
+            for j in range(5):
+                microbit.display.set_pixel(i, j, dots[i][j])
+                dots[i][j] = max(dots[i][j] - 1, 0)
+        #microbit.sleep(10)
         note = str(random.choice(letters)) + str(random.choice(numbersHigh)) + ":" + str(random.choice(numbersLow))
         music.play(note)
-        
+  
 if __name__ == "__main__":
     main()
